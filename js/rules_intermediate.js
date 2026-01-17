7800bce3e73256e399289aa2ecc0d5a2160c056c
二叉树树 <acofork@qq.com>
2026-01-17 18:49:29 +0800

    Add short link: M2SRkpx

--- js/rules_intermediate.js
+++ js/rules_intermediate.js
@@ -2290,5 +2290,9 @@ window.RULES_INTERMEDIATE = {
     "/oX21cdS": {
         "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
         "expired_at": "2026-01-21T17:00:00.000Z"
+    },
+    "/M2SRkpx": {
+        "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
+        "expired_at": "2026-01-21T17:00:00.000Z"
     }
 };
