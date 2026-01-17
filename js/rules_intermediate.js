56dafff0bb8b510952f044dfe5d653cf8a8c1223
二叉树树 <acofork@qq.com>
2026-01-17 19:25:44 +0800

    Add short link: florcn

--- js/rules_intermediate.js
+++ js/rules_intermediate.js
@@ -2758,5 +2758,9 @@ window.RULES_INTERMEDIATE = {
     "/KBkJR8n": {
         "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
         "expired_at": "2026-01-21T17:00:00.000Z"
+    },
+    "/florcn": {
+        "url": "https://flor.cn",
+        "expired_at": "2026-01-21T11:25:00.000Z"
     }
 };
