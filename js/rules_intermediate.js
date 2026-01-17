40a6deeb4086a21acfcc7da071822034e5533430
二叉树树 <acofork@qq.com>
2026-01-17 18:56:08 +0800

    Add short link: xRMBEkh

--- js/rules_intermediate.js
+++ js/rules_intermediate.js
@@ -2518,5 +2518,9 @@ window.RULES_INTERMEDIATE = {
     "/216AzBu": {
         "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
         "expired_at": "2026-01-21T17:00:00.000Z"
+    },
+    "/xRMBEkh": {
+        "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
+        "expired_at": "2026-01-21T17:00:00.000Z"
     }
 };
