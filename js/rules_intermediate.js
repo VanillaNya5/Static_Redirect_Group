ffdfdb2a76ed6be36c27751b5732b114e6099d16
二叉树树 <acofork@qq.com>
2026-01-17 18:56:10 +0800

    Add short link: mC20iBV

--- js/rules_intermediate.js
+++ js/rules_intermediate.js
@@ -2534,5 +2534,9 @@ window.RULES_INTERMEDIATE = {
     "/R7m5UMe": {
         "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
         "expired_at": "2026-01-21T17:00:00.000Z"
+    },
+    "/mC20iBV": {
+        "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
+        "expired_at": "2026-01-21T17:00:00.000Z"
     }
 };
