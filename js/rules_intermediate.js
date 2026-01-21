d21c1d24c18b81b243396c58ad2ff79f022c366b
二叉树树 <acofork@qq.com>
2026-01-21 20:13:25 +0800

    Add short link: nwic0r1

--- js/rules_intermediate.js
+++ js/rules_intermediate.js
@@ -7250,5 +7250,9 @@ window.RULES_INTERMEDIATE = {
     "/15xw7vo3t6": {
         "url": "https://cv.notaone.cn/",
         "expired_at": "2026-01-27T12:13:23.000Z"
+    },
+    "/nwic0r1": {
+        "url": "https://cv.notaone.cn/",
+        "expired_at": "2026-01-27T12:13:25.000Z"
     }
 };
