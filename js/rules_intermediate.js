6c1affaa8eb4d76a1e0fde8738208d4081799e79
二叉树树 <acofork@qq.com>
2026-01-24 13:28:06 +0800

    Add short link: cad0205c5a

--- js/rules_intermediate.js
+++ js/rules_intermediate.js
@@ -8350,5 +8350,9 @@ window.RULES_INTERMEDIATE = {
     "/00762982cb": {
         "url": "https://www.bilibili.com/video/av180",
         "expired_at": "2026-01-30T05:27:58.000Z"
+    },
+    "/cad0205c5a": {
+        "url": "https://www.bilibili.com/video/av182",
+        "expired_at": "2026-01-30T05:28:04.000Z"
     }
 };
